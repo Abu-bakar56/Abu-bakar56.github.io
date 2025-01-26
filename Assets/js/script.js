645c24652956a18fc9fd8552951a3b4da99b40d2
Abu-bakar56 <abubakarshahzad730@gmail.com>
2025-01-27 01:01:40 +0500

    update the web

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -106,7 +106,7 @@ ScrollReveal().reveal('.home-content p', ' .about-content', { origin:'right' });
 
 
 const typed = new Typed ('.multiple-text',{
-    strings:['AI Engineer'],
+    strings:['AI Engineer','Data Scientist','AI Engineer'],
     typeSpeed:100,
     backSpeed:100,
     backDelay:1000,
